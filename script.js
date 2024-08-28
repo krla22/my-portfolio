f50bea977d5efa37469e7d3a98dced19a4698b74
krla22 <kurtrobin22@gmail.com>
2024-08-28 14:05:10 +0800

    Portfolio barebones

---
+++ script.js
@@ -0,0 +1,43 @@
+function checkSkinABCsWebsite() {
+    window.open("https://krla22.github.io/SABC4thYear/index.html", "_blank")
+}
+
+function checkSkinABCsApp() {
+    window.open("https://www.canva.com/design/DAGFK536mNI/gDk-4gdRpN8EDDccE937WQ/view?utm_content=DAGFK536mNI&utm_campaign=designshare&utm_medium=link&utm_source=editor", "_blank")
+}
+
+function checkDesksenseApp() {
+    window.open("https://drive.google.com/file/d/1SSEmXaS5iSxkdTG2fKPXG6tLshJlCYYH/view?usp=sharing", "_blank")
+}
+
+function checkChromaGame() {
+    window.open("https://github.com/krla22/GameDevChroma", "_blank")
+}
+
+function checkRPSGame() {
+    window.open("https://krla22.github.io/new-rps/","_blank")
+}
+
+function checkDrawingGame() {
+    window.open("https://krla22.github.io/etch-sketch/","_blank")
+}
+
+function checkBasicCalculator() {
+    window.open("https://krla22.github.io/calculator-odin/","_blank")
+}
+
+function checkEditedVideos() {
+    window.open("https://drive.google.com/drive/folders/1a8KT86gn5cr22nF8n7VlyxouNS4lc_SH?usp=sharing", "_blank")
+}   
+
+function checkGraphicDesigns() {
+    window.open("https://drive.google.com/drive/folders/1vPfZg3HbgDFyiLZ049YQGN13k5JRkP8U?usp=sharing", "_blank")
+}
+
+function checkFBAcc() {
+    window.open("https://www.facebook.com/kurtrobin22", "_blank")
+}
+
+function checkGithubAcc() {
+    window.open("https://github.com/krla22", "_blank")
+}
